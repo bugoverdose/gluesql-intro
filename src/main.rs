b3d2eaf38a8c7314e193fecfb879888a51afc0c5
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-13 13:36:33 +0900

    feat: check gluesql functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use gluesql::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let storage = SledStorage::new("data/doc-db").unwrap();
+    let mut glue = Glue::new(storage);
+    let sqls = vec![
+        "DROP TABLE IF EXISTS Glue;",
+        "CREATE TABLE Glue (id INTEGER, name TEXT);",
+        "INSERT INTO Glue VALUES (1, '최지석'), (2, '문태훈'), (3, '위성훈'), (4, '조형관');",
+        "SELECT name FROM Glue WHERE id in (2,4);",
+    ];
+
+    for sql in sqls {
+        let output = glue.execute(sql).unwrap();
+        println!("{:?}", output)
+    }
 }
